fb1ff3f8a48db6e1c373c1a53ca9909e7cc5431b
Hugo Bois <hugo.bois@hotmail.fr>
2023-01-26 16:35:59 +0100

    feat: added kart prefab and fixed main menu UI

--- Assets/UI/Scripts/OptionsMenuUI.cs
+++ Assets/UI/Scripts/OptionsMenuUI.cs
@@ -4,15 +4,6 @@ public class OptionsMenuUI : MonoBehaviour
 {
     [SerializeField] private TMPro.TMP_Dropdown resolutionDropdown;
 
-    private void Start()
-    {
-        for (int i = Screen.resolutions.Length - 1; i > 0; i--)
-        {
-            Debug.Log(i);
-            resolutionDropdown.options[i].text = Screen.resolutions[i].width + "x" + Screen.resolutions[i].height;
-        }
-    }
-
     public void ToggleFullscreen()
     {
         Screen.fullScreen = !Screen.fullScreen;
@@ -21,11 +12,5 @@ public class OptionsMenuUI : MonoBehaviour
     public void SetFullscreenMode(int mode)
     {
         Screen.fullScreenMode = (FullScreenMode)(mode + 1);
-    }
-
-    public void SetResolution(int resolutionIndex)
-    {
-        Resolution resolution = Screen.resolutions[resolutionIndex];
-        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
