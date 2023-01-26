27290dabc7a32a4cec43dcb9324c704571dcd956
Jeremy Croiset <jeremy.croiset@epita.fr>
2023-01-26 21:22:18 +0100

    fix: render non-VR camera

---
+++ Assets/Scripts/NonVRViewSetup.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpectatorView : MonoBehaviour
+{
+    public Camera spectatorCamera;
+    private RawImage rawImage;
+
+    void Start()
+    {
+        if (this.GetComponent<RectTransform>() == null)
+        {
+            Debug.LogError("Script must be attached to a UI element with a RectTransform component.");
+        }
+
+        rawImage = this.GetComponent<RawImage>();
+        if (rawImage == null)
+        {
+            Debug.LogError("Script must be attached to a UI element with a RawImage component.");
+        }
+
+        OnRectTransformDimensionsChange();
+    }
+
+    private void OnRectTransformDimensionsChange()
+    {
+        if (spectatorCamera != null && rawImage != null)
+        {
+            spectatorCamera.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+            rawImage.texture = spectatorCamera.targetTexture;
+        }
+    }
+}
